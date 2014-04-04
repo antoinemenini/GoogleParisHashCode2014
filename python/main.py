60a1a1017da9791882bed46c2cbee26c44b7cc21
Thomas Moreau <toma.moral@gmail.com>
2014-04-04 20:31:36 +0200

    example parser

--- python/main.py
+++ python/main.py
@@ -9,5 +9,9 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Google Hash Code in Paris!')
+    parser.add_argument('--hw', action='store_true')
     args = parser.parse_args()
+
+    if args.hw:
+        print('Hello world!')
     main()
