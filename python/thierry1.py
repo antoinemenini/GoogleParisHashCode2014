cb82907cf23740afb7cbd55835bbc804083f2e5a
Thierry Deo <thierry.deo@gmail.com>
2014-04-04 21:45:47 +0200

    Algo malin (tentative)

--- python/thierry1.py
+++ python/thierry1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import sys
 
 def compte(arr):
     n = arr.shape[0]
@@ -35,6 +36,7 @@ def biggestSquareIntel(arr,i,j):
     p = arr.shape[1]
     ok = (arr[i][j]==1)
     s=0
+    score=0
     while ok:
         s+=1
         if (i+s<n) and (j+s<p) and (i>=s) and (j>=s):
@@ -42,15 +44,62 @@ def biggestSquareIntel(arr,i,j):
                 count = 0
                 if arr[i+k][j+s]==0:
                     count+=1
+                elif arr[i+k][j+s]==1:
+                    score+=1
                 if arr[i+k][j-s]==0:
                     count+=1
+                elif arr[i+k][j-s]==1:
+                    score+=1
                 if arr[i+s][j+k]==0:
                     count+=1
+                elif arr[i+s][j+k]==1:
+                    score+=1
                 if arr[i-s][j+k]==0:
                     count+=1
+                elif arr[i-s][j+k]==1:
+                    score+=1
             if(count > s/2):
                 ok=False
-    return [s-1,i,j]
+    return [score,s-1,i,j]
+
+def iterMalin(arr):
+    bg_tab = []
+    n, m = arr.shape
+    for i in range(n):
+        for j in range(m):
+            #if j % 100 == 0:
+            #    sys.stdout.write('.')
+            #    sys.stdout.flush()
+            if arr[i, j] == 1:
+                bg_tab.append(biggestSquareIntel(arr, i, j))
+    rank = np.array(bg_tab)
+    ind = rank[:, 0].argsort()
+    ranked_actions = rank[ind[::-1]]
+    current = arr.copy()
+    score,s,i,j = ranked_actions[0]
+    for p in range(i-s,i+s+1):
+        for q in range(j-s,j+s+1):
+            if current[p][q]==0:
+                current[p][q]=-1
+            if current[p][q]==1:
+                current[p][q]=2
+    print "PAINTSQ",i,j,s
+    return [score, current]
+
+def cleanWhite(arr):
+    n = arr.shape[0]
+    p = arr.shape[1]
+    for i in range(n):
+        for j in range(p):
+            if arr[i][j]==-1:
+                print "ERASECELL",i,j
+
+def solMalin(arr):
+    score, tab = iterMalin(arr)
+    while score>0:
+        score, tab = iterMalin(tab)
+    cleanWhite(tab)
+
 
 
 if __name__=='__main__':
