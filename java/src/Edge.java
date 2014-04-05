7697c88f94eba31113886110903ec457ba14bbbf
Guillaume Wenzek <guillaume.wenzek@polytechnique.org>
2014-04-05 11:40:04 +0200

    graph loader

---
+++ java/src/Edge.java
@@ -0,0 +1,14 @@
+/**
+ * Created by guillaume on 4/5/14.
+ */
+public class Edge {
+    public final int begin, end;
+    public final int cost, length;
+
+    public Edge(int b, int e, int c, int l){
+        begin = b;
+        end = e;
+        cost = c;
+        length = l;
+    }
+}
--- java/src/Main.java
+++ java/src/Main.java
@@ -2,9 +2,7 @@
  * Created by guillaume on 4/5/14.
  */
 public class Main {
-
     public static void main(String[] args){
-
+        Graph paris = new Graph("paris_54000.txt");
     }
-    
 }
